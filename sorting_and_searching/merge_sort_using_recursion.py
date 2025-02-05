c89c993c4823926da945f963663c94fcbd8a2523
Agam-Patel-DS <colabwitagam@gmail.com>
2025-02-05 13:39:13 +0000

    Merge Sort and Binary Search Practice

--- sorting_and_searching/merge_sort_using_recursion.py
+++ sorting_and_searching/merge_sort_using_recursion.py
@@ -1,21 +1,50 @@
-# Merge Sort
-def merge(l1,s,m,e):
-  pass
+def merge(l1, s, m, e):
+    i = s
+    j = m + 1
+    ans = []
 
-def merge_sort_helper(l1,s,e):
-  if (s>=e):
-    return
+    # Merging two sorted halves
+    while i <= m and j <= e:
+        if l1[i] < l1[j]:
+            ans.append(l1[i])
+            i += 1
+        else:
+            ans.append(l1[j])
+            j += 1
 
-  mid=s+(e-s)//2
+    # Append remaining elements from the left half
+    while i <= m:
+        ans.append(l1[i])
+        i += 1
 
-  merge_sort_helper(l1,s,mid)
-  merge_sort_helper(l1,mid+1,e)
+    # Append remaining elements from the right half
+    while j <= e:
+        ans.append(l1[j])
+        j += 1
 
-  merge(l1,s,mid,e)
-  return
+    # Copy sorted elements back into original list
+    l1[s:e+1] = ans  
+
+
+def merge_sort_helper(l1, s, e):
+    if s >= e:
+        return  
+
+    mid = s + (e - s) // 2  
+
+    # Recursively divide the array
+    merge_sort_helper(l1, s, mid)
+    merge_sort_helper(l1, mid + 1, e)
+
+    # Merge the sorted halves
+    merge(l1, s, mid, e)
+
 
 def merge_sort(l1):
-  return merge_sort_helper(l1,0,len(l1))
+    merge_sort_helper(l1, 0, len(l1) - 1)  #  No return needed
 
-l1=[23,54,12,52,65,21]
-print(merge_sort(l1))
+
+# Test  function
+l1 = [23, 54, 12, 52, 65, 21]
+merge_sort(l1)  # Sorting happens in place
+print(l1)  #  Now it will correctly print: [12, 21, 23, 52, 54, 65]
